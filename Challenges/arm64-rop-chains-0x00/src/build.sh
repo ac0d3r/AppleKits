880e13319900c6f93ce5f157bfa64adc972e20c3
ac0d3r <admin@imipy.com>
2024-07-24 14:08:49 +0800

    remove Challenges

--- Challenges/arm64-rop-chains-0x00/src/build.sh
+++
@@ -1,6 +0,0 @@
-`xcrun --sdk iphoneos -f clang` -arch arm64 \
-      -isysroot `xcrun --sdk iphoneos --show-sdk-path` \
-      -miphoneos-version-min=8.0 \
-      -o dontpopme \
-      dontpopme.c
-`xcrun --sdk iphoneos -f codesign` -f -s - --entitlements entitlements.xml dontpopme
